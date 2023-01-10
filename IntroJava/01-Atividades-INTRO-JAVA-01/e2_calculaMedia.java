d5eea42bb2da2bc3751b2f61b583f94f349a8ea1
Gustavo Balieiro <gbalieirof@gmail.com>
2023-01-10 11:08:16 -0300

    Ex02 - Calcula a média de 4 notas

---
+++ IntroJava/01-Atividades-INTRO-JAVA-01/e2_calculaMedia.java
@@ -0,0 +1,25 @@
+import static java.lang.System.out;
+import java.util.Scanner;
+
+public class e2_calculaMedia {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        float[] fNotas = new float[4];
+        float fSoma=0f, fMedia;
+
+        for(int c=0; c < 4; c++){
+            out.printf("Digite a nota do %dº Nota: ", c+1);
+            fNotas[c] = entrada.nextFloat();
+            fSoma += fNotas[c];
+        }
+        fMedia = fSoma/4;
+        if(fMedia < 7){
+            out.println("EM RECUPERACAO!");
+        } else if (fMedia < 5) {
+            out.println("REPROVADO!");
+        }else {
+            out.println("APROVADO");
+        }
+        out.printf("Média: %.1f", fMedia);
+    }
+}
