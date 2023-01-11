7dabb255ce80d6529832fcb5d891a19931ed857a
Gustavo Balieiro <gbalieirof@gmail.com>
2023-01-11 17:34:11 -0300

    Ex04 - Verifica se para idade é obrigatoria ou facultativo a votar

---
+++ IntroJava/02-Atividades-INTRO-JAVA-05/e4_verificaAptoVotar.java
@@ -0,0 +1,23 @@
+package JAVA_05;
+import static java.lang.System.out;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class e4_verificaAptoVotar {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int iIdade;
+
+        out.print("Digite sua idade: ");
+        iIdade = entrada.nextInt();
+
+        if(iIdade < 16){
+            out.println("Näo apto a votar");
+        } else if (iIdade < 18 || iIdade >= 65) {
+            out.println("Apto a votar, voto é facultativo");
+        }else {
+            out.println("Apto a votar, voto é obrigatório");
+        }
+    }
+}
